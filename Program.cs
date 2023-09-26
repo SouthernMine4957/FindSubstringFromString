eee87922070069f185ba9849c0061647596089fc
SouthernMine4957 <112040234+SouthernMine4957@users.noreply.github.com>
2023-09-26 19:52:03 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the main string:");
+        string mainString = Console.ReadLine();
+
+        Console.WriteLine("Enter the substring to find:");
+        string subString = Console.ReadLine();
+
+        int index = mainString.IndexOf(subString);
+
+        if (index != -1)
+        {
+            Console.WriteLine($"The substring '{subString}' was found at index {index} in the main string.");
+        }
+        else
+        {
+            Console.WriteLine($"The substring '{subString}' was not found in the main string.");
+        }
+    }
+}
